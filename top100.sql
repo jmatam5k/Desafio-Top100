f349888480fb817c8b86d8e159a7e675bbbffa4d
Jennifer Matamala <jmatam.go@gmail.com>
2020-09-21 00:45:44 -0300

    respuestas 8,9 y 10

--- top100.sql
+++ top100.sql
@@ -3,7 +3,7 @@ CREATE DATABASE peliculas;
 
 -- 2. Revisar los archivos peliculas.csv y reparto.csv para crear las tablas correspondientes,
 --determinando la relación entre ambas tablas. 
---Determiné que la relación es el id de la pelicula
+--Respuesta: Determiné que la relación es el id de la pelicula
 
 -- 3. Cargar ambos archivos a su tabla correspondiente (1 punto)
 CREATE TABLE peliculas(id INT, pelicula VARCHAR(60), anio_estreno INT, director VARCHAR(60), PRIMARY KEY(id));
@@ -12,7 +12,7 @@ CREATE TABLE reparto(id SERIAL, id_pelicula INT, actor VARCHAR(30), FOREIGN KEY 
 \copy reparto(id_pelicula, actor) FROM '/Users/jennifermatamalagodoy/Desktop/reparto.csv' csv header;
 
 --4. Listar todos los actores que aparecen en la película "Titanic", indicando el título de la película,
---año de estreno, director y todo el reparto. (0.5 puntos)
+--año de estreno, director y todo el reparto. 
 SELECT * FROM peliculas INNER JOIN reparto ON peliculas.id = reparto.id_pelicula WHERE pelicula = 'Titanic';
 
 -- 5. Listar los titulos de las películas donde actúe Harrison Ford.
@@ -23,3 +23,12 @@ SELECT director, COUNT(director) FROM peliculas GROUP BY director ORDER BY COUNT
 
 -- 7. Indicar cuantos actores distintos hay 
 SELECT COUNT(DISTINCT actor) FROM reparto;
+
+--8. Indicar las películas estrenadas entre los años 1990 y 1999 (ambos incluidos) ordenadas por título de manera ascendente.
+SELECT * FROM peliculas WHERE anio_estreno BETWEEN 1990 AND 1999 ORDER BY pelicula ASC;
+
+-- 9. Listar el reparto de las películas lanzadas el año 2001
+SELECT actor, pelicula FROM reparto JOIN peliculas ON reparto.id_pelicula = peliculas.id WHERE anio_estreno = 2001;
+
+-- 10. Listar los actores de la película más nueva 
+SELECT actor, pelicula, anio_estreno FROM reparto JOIN peliculas ON reparto.id_pelicula = peliculas.id ORDER BY peliculas.anio_estreno DESC LIMIT 1;
