82933bbca37712a7ed88276de898e90b486cffe6
Jennifer Matamala <jmatam.go@gmail.com>
2020-09-20 23:51:32 -0300

    Resolviendo 1, 2 y 3

---
+++ top100.sql
@@ -0,0 +1,11 @@
+--1. Crear base de datos llamada películas
+CREATE DATABASE peliculas;
+
+-- 2. Revisar los archivos peliculas.csv y reparto.csv para crear las tablas correspondientes,
+--determinando la relación entre ambas tablas. 
+--Determiné que la relación es el id de la pelicula
+
+-- 3. Cargar ambos archivos a su tabla correspondiente (1 punto)
+CREATE TABLE peliculas(id INT, pelicula VARCHAR(60), anio_estreno INT, director VARCHAR(60), PRIMARY KEY(id));
+\copy peliculas FROM '/Users/jennifermatamalagodoy/Desktop/peliculas.csv' csv header;
+CREATE TABLE reparto(id SERIAL, id_pelicula INT, actor VARCHAR(30), FOREIGN KEY (id_pelicula) REFERENCES peliculas(id)); 
