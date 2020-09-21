dbe71d9ccfdb72a056d5ad1a107d589cdb2a0d69
Jennifer Matamala <jmatam.go@gmail.com>
2020-09-21 00:00:06 -0300

    respuesta 4

--- top100.sql
+++ top100.sql
@@ -9,3 +9,8 @@ CREATE DATABASE peliculas;
 CREATE TABLE peliculas(id INT, pelicula VARCHAR(60), anio_estreno INT, director VARCHAR(60), PRIMARY KEY(id));
 \copy peliculas FROM '/Users/jennifermatamalagodoy/Desktop/peliculas.csv' csv header;
 CREATE TABLE reparto(id SERIAL, id_pelicula INT, actor VARCHAR(30), FOREIGN KEY (id_pelicula) REFERENCES peliculas(id)); 
+\copy reparto(id_pelicula, actor) FROM '/Users/jennifermatamalagodoy/Desktop/reparto.csv' csv header;
+
+--4. Listar todos los actores que aparecen en la película "Titanic", indicando el título de la película,
+--año de estreno, director y todo el reparto. (0.5 puntos)
+SELECT * FROM peliculas INNER JOIN reparto ON peliculas.id = reparto.id_pelicula WHERE pelicula = 'Titanic';
